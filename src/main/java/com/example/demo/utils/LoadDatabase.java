38e9efd46a28d245c03b7e447a2e9a4b6fb6b296
johana infesta <johana.infesta@gmail.com>
2022-10-19 12:42:36 -0300

    creacion de db automatica

--- src/main/java/com/example/demo/utils/LoadDatabase.java
+++ src/main/java/com/example/demo/utils/LoadDatabase.java
@@ -32,19 +32,22 @@ private static final Logger logger = LoggerFactory.getLogger(LoadDatabase.class)
 //Correr con MySQL 
 //conexion hecha en resources application.properties
 
-//	@Bean
-//	CommandLineRunner initEstudiante(@Qualifier("estudianteRepository") EstudianteRepository repository) {
-//		return args -> {
-//			logger.info("preloading" + repository.save(new Estudiante("Seba", "Perez", 20, "masculino", 40520425, "Olavarria")));
-//			logger.info("preloading" + repository.save(new Estudiante("Johana", "Infesta", 31, "femenino", 35580050, "Tandil")));
-//		};
-//	}
-//	
-//	
-//	@Bean
-//	CommandLineRunner initCarrera(@Qualifier("carreraRepository") CarreraRepository repository) {
-//		return args -> {
-//			logger.info("preloading" + repository.save(new Carrera("TUDAI", 3)));
-//		};
-//	}
+	@Bean
+	CommandLineRunner initEstudiante(@Qualifier("estudianteRepository") EstudianteRepository repository) {
+		return args -> {
+			logger.info("preloading" + repository.save(new Estudiante("Seba", "Perez", 20, "masculino", 40520425, "Olavarria")));
+			logger.info("preloading" + repository.save(new Estudiante("Juan", "Lopez", 20, "masculino", 40100410, "Tandil")));
+			logger.info("preloading" + repository.save(new Estudiante("Johana", "Infesta", 31, "femenino", 35580050, "Tandil")));
+		};
+	}
+	
+	
+	@Bean
+	CommandLineRunner initCarrera(@Qualifier("carreraRepository") CarreraRepository repository) {
+		return args -> {
+			logger.info("preloading" + repository.save(new Carrera("TUDAI", 3)));
+			logger.info("preloading" + repository.save(new Carrera("TUARI", 3)));
+			logger.info("preloading" + repository.save(new Carrera("Ingenieria en Sistemas", 5)));
+		};
+	}
 }
